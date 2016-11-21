5ff8d3b9a812750ceb479f89ae5a2c6f8eed6c61
Krishna Vudumula <RLE0401@rlpl.com>
2016-11-21 10:44:24 +0530

    rsyslog added

---
+++ jnj_rsyslog/attributes/default.rb
@@ -0,0 +1,2 @@
+##default[:log][:server] = "127.0.0.1"
+default[:log][:server] = '127.0.0.1:1514'
---
+++ jnj_rsyslog/recipes/default.rb
@@ -0,0 +1,20 @@
+#
+# Cookbook Name:: jnj_rsyslog
+# Recipe:: default
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+template '/etc/rsyslog.d/al_log_agt.conf' do
+  source 'al_log_agt.erb'
+  mode  '0644'
+  variables({
+    :LogServer => node[:log][:server]
+    })
+  notifies :restart, 'service[rsyslog]', :immediately
+end
+
+service 'rsyslog' do
+	action [:enable, :start]
+end
