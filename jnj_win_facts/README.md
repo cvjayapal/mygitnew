8a0ce488f1458ee89b7cf0f36b703438583441e0
Krishna Vudumula <RLE0401@rlpl.com>
2016-11-29 16:04:28 +0530

    Added win_facts cookbook

---
+++ jnj_win_facts/README.md
@@ -0,0 +1,37 @@
+# jnj_win_facts Cookbook
+
+ Collects the facts about the system based on the avaialable metadata and sets
+
+## Requirements
+### Platforms
+
+- windows-2008r2
+
+### Chef
+
+- Chef 12.0 or later
+
+### Cookbooks
+
+- version-0.1.0
+
+## Attributes
+
+## Usage
+
+### jnj_win_facts::default
+
+Just include `jnj_win_facts` in your node's `run_list`:
+
+{
+  "name":"my_node",
+  "run_list": [
+    "recipe[jnj_win_facts]"
+  ]
+}
+
+
+## License and Authors
+
+Authors: TODO: Krishna
+
---
+++ jnj_win_facts/recipes/default.rb
@@ -0,0 +1,62 @@
+#
+# Cookbook Name:: jnj_win_facts
+# Recipe:: default
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+# directory 'C:\\ProgramData\\PuppetLabs' do
+#         action :create
+#         owner  'Administrators'
+#         group  'Administrators'
+        
+# end
+
+# directory 'C:\\ProgramData\\PuppetLabs\\facter' do
+#         action :create
+#         owner  'Administrators'
+#         group  'Administrators'
+        
+# end
+
+# directory 'C:\\ProgramData\\PuppetLabs\\facter\\facts.d' do
+#         action :create
+#         owner  'Administrators'
+#         group  'Administrators'
+        
+# end
+
+%w[ C:\\ProgramData C:\\ProgramData\\PuppetLabs C:\\ProgramData\\PuppetLabs\\facter C:\\ProgramData\\PuppetLabs\\facter\\facts.d ].each do |path|
+  directory path do
+    owner  'Administrators'
+    group  'Administrators'
+    action :create
+  end
+end
+
+
+#%w[ C:\\ProgramData\\PuppetLabs\\facter\\facts.d ].each do |path|
+#  directory path do
+#  owner  'Administrators'
+#  group  'Administrators'
+#  action :create
+#   end
+#end
+
+
+
+cookbook_file 'C:\\ProgramData\\PuppetLabs\\facter\\facts.d\\scm_facts.ps1' do
+        source 'scm_facts.ps1'
+        owner  'Administrators'
+        group  'Administrators'
+        mode  0775
+        action :create       
+end
+
+file 'C:\\ProgramData\\PuppetLabs\\facter\\facts.d\\aws_facts.ps1' do
+        
+        action :delete        
+end
+
