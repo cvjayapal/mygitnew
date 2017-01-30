275a70900817ebbef2690641040151db751cc8b1
krish07d <krish07d@gmail.com>
2017-01-30 14:29:02 +0530

    Update default.rb
    changed attributes

--- jnj_win_vpcx_task_uploadmonitormetrics/attributes/default.rb
+++ jnj_win_vpcx_task_uploadmonitormetrics/attributes/default.rb
@@ -2,5 +2,3 @@ default['jnj_win_vpcx_task_uploadmonitormetrics']['directory'] = %w( C:\\Program
 default['scm_hosting']['platform'] = 'AWS'
 default['scm']['buildtype'] = 'buildworkflow'
 default['windowspowershell']['v1.0'] = 'C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe.config'
-default['win_jnj']['export_uploadmonitormetrics_ps1_path'] = "#{node['win_jnj']['vpcx_path']}\\upload-monitor-metrics.ps1"
-default['win_jnj']['export_uploadmonitormetrics_xml_path'] = "#{node['win_jnj']['vpcx_path']}\\UploadMonitorMetri"
