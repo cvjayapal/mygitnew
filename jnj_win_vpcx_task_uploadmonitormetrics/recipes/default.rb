073c70a4c64856b8aa47a64d6c7b3abd5fbcb71f
krish07d <krish07d@gmail.com>
2017-01-30 14:31:03 +0530

    modified recipe code

--- jnj_win_vpcx_task_uploadmonitormetrics/recipes/default.rb
+++ jnj_win_vpcx_task_uploadmonitormetrics/recipes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: jnj_win_vpcx_task_uploadmonitormetrics
 # Recipe:: default
 #
-# Copyright 2016, YOUR_COMPANY_NAME
+# Copyright 2016, Relevance Lab
 #
 # All rights reserved - Do Not Redistribute
 #
@@ -23,51 +23,51 @@ node['jnj_win_vpcx_task_uploadmonitormetrics']['directory'].each do |path|
 end
 
 if node['scm_hosting']['platform'] == 'AWS'
-  node['win_jnj']['vpcx_path'] = if node['scm']['buildtype'] == 'buildworkflow'
-                'C:\\ProgramData\\JnJ\\Its_core\\vpcx'
-              else
-                'C:\\Program Files\\vpcx'
-              end
+  @vpcx_path = if node['scm']['buildtype'] == 'buildworkflow'
+                 'C:\\ProgramData\\JnJ\\Its_core\\vpcx'
+               else
+                 'C:\\Program Files\\vpcx'
+               end
+  vpcx = @vpcx_path
+
   # Task: UploadMonitorMetrics
+
   export_uploadmonitormetrics_tasks_folder = 'vpcx\\UploadMonitorMetrics'
+  export_uploadmonitormetrics_ps1_path = "#{vpcx}\\upload-monitor-metrics.ps1"
+  export_uploadmonitormetrics_xml_path = "#{vpcx}\\UploadMonitorMetrics.xml"
+
   # 1. copy upload-monitor-metrics.ps1 and related scripts to $vpcx_path
-  cookbook_file node['win_jnj']['export_uploadmonitormetrics_ps1_path'] do
+
+  cookbook_file export_uploadmonitormetrics_ps1_path.to_s do
     source 'upload-monitor-metrics.ps1'
     action :create
   end
 
-  cookbook_file "#{vpcx_path}\\mon-put-metrics-mem.ps1" do
+  cookbook_file "#{vpcx}\\mon-put-metrics-mem.ps1" do
     source 'mon-put-metrics-mem.ps1'
     action :create
   end
 
-  cookbook_file "#{node[win_jnj']['vpcx_path']}\\mon-put-metrics-partitions.ps1" do
+  cookbook_file "#{vpcx}\\mon-put-metrics-partitions.ps1" do
     source 'mon-put-metrics-partitions.ps1'
     action :create
   end
 
-  cookbook_file "#{node[win_jnj']['vpcx_path']}\\mon-shared.ps1" do
+  cookbook_file "#{vpcx}\\mon-shared.ps1" do
     source 'mon-shared.ps1'
     action :create
   end
+
   # 2. copy UploadMonitorMetrics.xml.erb template to $vpcx_path and create scheduled task
+
   if node['scm']['buildtype'] == 'buildworkflow'
-    template node['win_jnj']['export_uploadmonitormetrics_xml_path'] do
+    template export_uploadmonitormetrics_xml_path.to_s do
       source 'UploadMonitorMetrics.xml.erb'
       action :create
     end
-    # execute'Task_UploadMonitorMetrics' do
-    #   creates "C:\\Windows\\System32\\Tasks\\'#{export_uploadmonitormetrics_tasks_folder}'"
-    #   command "schtasks /create /f /tn \'#{export_uploadmonitormetrics_tasks_folder}\' /xml \'#{node['win_jnj']['export_uploadmonitormetrics_xml_path']}\'"
-    #   command "schtasks /create /f /tn \'#{export_uploadmonitormetrics_tasks_folder}\' /xml \'#{node['win_jnj']['export_uploadmonitormetrics_xml_path']}\'"
-    #   provider Chef::Provider::Execute
-    #   returns 0
-    # end
     powershell_script 'Task_UploadMonitorMetrics' do
       creates "C:\\Windows\\System32\\Tasks\\'#{export_uploadmonitormetrics_tasks_folder}'"
-      command "schtasks /create /f /tn \'#{export_uploadmonitormetrics_tasks_folder}\' /xml \'#{node['win_jnj']['export_uploadmonitormetrics_xml_path']}\'"
-      command "schtasks /create /f /tn \'#{export_uploadmonitormetrics_tasks_folder}\' /xml \'#{node['win_jnj']['export_uploadmonitormetrics_xml_path']}\'"
-      provider Chef::Provider::PowershellScript
+      command "schtasks /create /f /tn \'#{export_uploadmonitormetrics_tasks_folder}\' /xml \'#{export_uploadmonitormetrics_xml_path}\'"
       returns 0
     end
   end
